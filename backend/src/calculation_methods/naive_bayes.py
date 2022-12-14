d3e360cbe56afb5c3039e6cb913156b5cc938a23
Adriana Boscolo <adrianaboscolo1@gmail.com>
2022-12-14 14:18:04 +0100

    fixes to prediction display

--- backend/src/calculation_methods/naive_bayes.py
+++ backend/src/calculation_methods/naive_bayes.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 from .data_processing import DataProcessing, fig2img
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.naive_bayes import GaussianNB
 from sklearn import metrics
 
@@ -16,7 +15,7 @@ def naive_bayes(data, classes):
 
     #get sample for prediction error checking
     sample = data.loc[:, data.columns != classes]
-    sample = sample.iloc[:1,:]
+    sample = sample.iloc[:1, :]
 
     #transform class labels to allow for pca
     data = data.to_numpy()
@@ -27,7 +26,7 @@ def naive_bayes(data, classes):
 
     dataX, dataY = data.loc[:, data.columns != classes], data[classes]
 
-    #split and standardize data
+    #split data
     trX, teX, trY, teY = train_test_split(dataX, dataY, test_size = 0.2, random_state = 0)
 
     #Gaussian Naive-Bayes
@@ -36,11 +35,19 @@ def naive_bayes(data, classes):
     predY = gnb.predict(teX)
 
     #return predictions for test set
-    preds = pd.DataFrame(predY, columns = ["Predicted " + classes])
-    target = pd.DataFrame(teY)
-    preds.index = target.index
-    result["file"] = pd.concat([teX, target, preds], axis=1)
+    preds = pd.DataFrame(label[pd.DataFrame(predY, columns = [classes])], columns = ["Predicted " + classes])
+    target = pd.DataFrame(label[pd.DataFrame(teY)], columns = [classes])
+
+    #calculate accuracy on test data
+    accuracy = pd.DataFrame([str(metrics.accuracy_score(teY, predY) * 100) + "%"], columns=["Accuracy"])
+
+    #reindex
+    target.index= preds.index = teX.index
+    accuracy.index = np.arange(teX.iloc[0].name, len(accuracy)+int(teX.iloc[0].name))
+
+    result["file"] = pd.concat([teX, target, preds, accuracy], axis=1)
     result["model"] = gnb
     result["original_file_sample"] = sample
+    result["labels"] = label
 
-    return [result]
+    return result
